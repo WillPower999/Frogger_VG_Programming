b6ea2633decd6fb70d07c1952eeb795510cc8dc9
WUStudent <wustudent@webster.edu>
2021-04-15 16:20:30 -0500

    Scoring

---
+++ Gorloker/Assets/Scripts/ScoringPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringPlayer : MonoBehaviour
+{
+    public GameObject collectImage;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+            //addpoints
+
+            Destroy(gameObject);
+
+            GameObject newGameObject = Instantiate(collectImage);
+            newGameObject.transform.position = transform.position;
+
+        }
+    }
+
+}
