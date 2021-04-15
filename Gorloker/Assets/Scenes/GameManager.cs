cc85464f5d4d522d5d1c682a7b027fd82507d70e
WUStudent <wustudent@webster.edu>
2021-04-15 15:23:00 -0500

    GAME MANAGER RESET LEVEL

--- Gorloker/Assets/Scenes/GameManager.cs
+++ Gorloker/Assets/Scenes/GameManager.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HUD : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Vector3 playerStartPosition;
+    public GameObject Player;
+
+
+    void resetLevel()
+    {
+        Player.transform.position = playerStartPosition;
+
+    }
+
+
     void Start()
     {
-        
+        playerStartPosition = Player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            resetLevel();
+
+        }
     }
 }
