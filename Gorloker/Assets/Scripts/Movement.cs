8d672dabd999d16c1397e08462b6912a02dcc2b9
WUStudent <wustudent@webster.edu>
2021-04-29 14:26:12 -0500

    sprites

--- Gorloker/Assets/Scripts/Movement.cs
+++ Gorloker/Assets/Scripts/Movement.cs
@@ -9,23 +9,54 @@ public class Movement : MonoBehaviour
     private float timeToMove = 0.13f;
     private float elapsedTime = 0;
 
+    public SpriteRenderer playerSprite;
+    public Sprite walkLeft;
+    public Sprite walkRight;
+    public Sprite stand;
+
+    private bool isWalkLeft = false;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) && !isMoving)
+        {
+            playerSprite.transform.eulerAngles = new Vector3(0, 0, 0);
             StartCoroutine(MovePlayer(Vector3.up));
 
+        }
+
         if (Input.GetKeyDown(KeyCode.A) && !isMoving)
+        {
+            playerSprite.transform.eulerAngles = new Vector3(0, 0, 90);
             StartCoroutine(MovePlayer(Vector3.left));
+        }
 
         if (Input.GetKeyDown(KeyCode.S) && !isMoving)
+        {
+            playerSprite.transform.eulerAngles = new Vector3(0, 0, 180);
             StartCoroutine(MovePlayer(Vector3.down));
+        }
 
         if (Input.GetKeyDown(KeyCode.D) && !isMoving)
+        {
+            playerSprite.transform.eulerAngles = new Vector3(0, 0, -90);
             StartCoroutine(MovePlayer(Vector3.right));
+        }
     }
     private IEnumerator MovePlayer(Vector3 direction)
     {
         isMoving = true;
+
+        if (isWalkLeft)
+        {
+            playerSprite.sprite = walkLeft;
+            isWalkLeft = false;
+        }
+        else
+        {
+            playerSprite.sprite = walkRight;
+            isWalkLeft = true;
+        }
 
         elapsedTime = 0;
 
@@ -41,6 +72,13 @@ public class Movement : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+
+        yield return new WaitForSeconds(.1f);
+
+        if (!isMoving)
+        {
+            playerSprite.sprite = stand;
+        }
     }
 
     public void CancelMove()
